8a3613cadd41f01788a33ac3ad287f1d6bdc767d
JFEMENIA <jorgefemenia@gmail.com>
2024-04-23 20:46:54 -0300

    js clase 13

---
+++ Clases/Front/3.Javascript/Clase13/script.js
@@ -0,0 +1,34 @@
+// comentario de linea
+
+/*
+comentario
+de bloque
+*/
+
+// variables
+var num1 = 3
+let num2 = 5
+const num3 = 7
+
+
+// operadores aritmeticos
+const suma = num1 + num2
+const resta = num3 - num1
+const multiplicacion = num2 * num1
+const division = num2 / num1
+const modulo = num2 % num1
+
+// mostrar variable en consola
+console.log("La suma da: ", suma); // 8
+console.log("La resta da: ", resta); // 4
+console.log("La multiplicacion da: ", multiplicacion); // 15
+console.log("La división da: ", division); // 1.66
+console.log("El modulo da: ", modulo); // 2
+console.log(`El incremento de ${num1} es: ${++num1}`); // 4
+console.log(`El decremento de ${num1} es: ${--num1}`) // 3
+
+
+
+
+
+
